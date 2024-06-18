0b5f46c02b15a2832025f9b01764125c21e782be
BrianFaviansa <brianfpd31@gmail.com>
2024-06-18 10:50:01 +0700

    feat: prepare for atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React from "react";
+import Button from "./components/Elements/Button";
+
+
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello World</div>
+      <div className="flex gap-x-4">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Register</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
