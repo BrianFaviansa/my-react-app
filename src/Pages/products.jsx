72e16f2d8ae3f559b054fef5a6564c9a337e6c92
BrianFaviansa <brianfpd31@gmail.com>
2024-06-18 14:41:55 +0700

    feat: rendering list card

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,29 +1,50 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    image: "/images/shoes-1.jpg",
+    price: "2.500.000",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias illum
+    distinctio natus sunt, consequuntur nemo, illo eaque beatae cumque
+    odio voluptas, porro voluptatibus aut perferendis libero cum minima
+    sapiente animi.`,
+  },
+  {
+    id: 2,
+    name: "Kucing Baru",
+    image: "/images/cat-1.jpg",
+    price: "3.500.000",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias illum
+    distinctio natus sunt, consequuntur nemo, illo eaque beatae cumque
+    odio voluptas, porro voluptatibus aut perferendis libero cum minima
+    sapiente animi.`,
+  },
+  {
+    id: 3,
+    name: "Minyak Filma",
+    image: "/images/minyak filma.jpg",
+    price: "360.000",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias illum
+    distinctio natus sunt, consequuntur nemo, illo eaque beatae cumque
+    odio voluptas, porro voluptatibus aut perferendis libero cum minima
+    sapiente animi.`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias illum
-          distinctio natus sunt, consequuntur nemo, illo eaque beatae cumque
-          odio voluptas, porro voluptatibus aut perferendis libero cum minima
-          sapiente animi.
-        </CardProduct.Body>
-        <CardProduct.Footer price="2.500.000" />
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="/images/cat-1.jpg" />
-        <CardProduct.Body title="Kucing Geming">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias illum
-          distinctio natus sunt, consequuntur nemo, illo eaque beatae cumque
-          odio voluptas, porro voluptatibus aut perferendis libero cum minima
-          sapiente animi.
-        </CardProduct.Body>
-        <CardProduct.Footer price="10.000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
       {children}
     </div>
   );
@@ -12,18 +12,18 @@ const CardProduct = (props) => {
 const Header = (props) => {
   const { image } = props;
   return (
-    <a className="flex justify-center" href="">
-      <img src={image} alt="Product" className="p-8 max-h-64 rounded-t-lg" />
+    <a href="">
+      <img src={image} alt="Product" className="p-8 rounded-t-lg" />
     </a>
   );
 };
 const Body = (props) => {
-  const { title, children } = props;
+  const { name, children } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {title}
+          {name}
         </h5>
         <p className="text-sm text-white">{children}</p>
       </a>
